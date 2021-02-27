4d04e014235a24372cd23c7e98c60d7e44f1eb92
yitangsun <53280136+yitangsun@users.noreply.github.com>
2021-02-26 19:34:41 -0500

    Create Download_WBC.sh

---
+++ Download_WBC.sh
@@ -0,0 +1,60 @@
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/VuckovicD_32888494_GCST90002394/GCST90002394_buildGRCh37.tsv
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/VuckovicD_32888494_GCST90002382/GCST90002382_buildGRCh37.tsv
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/VuckovicD_32888494_GCST90002389/GCST90002389_buildGRCh37.tsv
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/VuckovicD_32888494_GCST90002380/GCST90002380_buildGRCh37.tsv
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/VuckovicD_32888494_GCST90002379/GCST90002379_buildGRCh37.tsv
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/VuckovicD_32888494_GCST90002399/GCST90002399_buildGRCh37.tsv
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/VuckovicD_32888494_GCST90002388/GCST90002388_buildGRCh37.tsv
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/VuckovicD_32888494_GCST90002393/GCST90002393_buildGRCh37.tsv
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/VuckovicD_32888494_GCST90002398/GCST90002398_buildGRCh37.tsv
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/VuckovicD_32888494_GCST90002407/GCST90002407_buildGRCh37.tsv
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/VuckovicD_32888494_GCST90002381/GCST90002381_buildGRCh37.tsv
+
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/ChenMH_32888493_GCST90002292/GCST90002292_buildGRCh37.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/ChenMH_32888493_GCST90002298/GCST90002298_buildGRCh37.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/ChenMH_32888493_GCST90002351/GCST90002351_buildGRCh37.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/ChenMH_32888493_GCST90002340/GCST90002340_buildGRCh37.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/ChenMH_32888493_GCST90002316/GCST90002316_buildGRCh37.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/ChenMH_32888493_GCST90002374/GCST90002374_buildGRCh37.tsv.gz
+
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004600/harmonised/27863252-GCST004600-EFO_0007991.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004631/harmonised/27863252-GCST004631-EFO_0007992.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004618/harmonised/27863252-GCST004618-EFO_0005090.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004624/harmonised/27863252-GCST004624-EFO_0005090.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004632/harmonised/27863252-GCST004632-EFO_0007993.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004627/harmonised/27863252-GCST004627-EFO_0004587.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004625/harmonised/27863252-GCST004625-EFO_0005091.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004623/harmonised/27863252-GCST004623-EFO_0007994.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004613/harmonised/27863252-GCST004613-EFO_0004833.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004620/harmonised/27863252-GCST004620-EFO_0004833.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004626/harmonised/27863252-GCST004626-EFO_0007988.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004610/harmonised/27863252-GCST004610-EFO_0004308.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004606/harmonised/27863252-GCST004606-EFO_0004842.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004633/harmonised/27863252-GCST004633-EFO_0007990.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004629/harmonised/27863252-GCST004629-EFO_0004833.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004617/harmonised/27863252-GCST004617-EFO_0007996.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004609/harmonised/27863252-GCST004609-EFO_0007989.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004634/harmonised/27863252-GCST004634-EFO_0007995.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004614/harmonised/27863252-GCST004614-EFO_0007987.h.tsv.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/AstleWJ_27863252_GCST004608/harmonised/27863252-GCST004608-EFO_0007997.h.tsv.gz
+
+mv 27863252-GCST004600-EFO_0007991.h.tsv 27863252-GCST004600.tsv
+mv 27863252-GCST004606-EFO_0004842.h.tsv 27863252-GCST004606.tsv
+mv 27863252-GCST004608-EFO_0007997.h.tsv 27863252-GCST004608.tsv
+mv 27863252-GCST004609-EFO_0007989.h.tsv 27863252-GCST004609.tsv
+mv 27863252-GCST004610-EFO_0004308.h.tsv 27863252-GCST004610.tsv
+mv 27863252-GCST004613-EFO_0004833.h.tsv 27863252-GCST004613.tsv
+mv 27863252-GCST004614-EFO_0007987.h.tsv 27863252-GCST004614.tsv
+mv 27863252-GCST004617-EFO_0007996.h.tsv 27863252-GCST004617.tsv
+mv 27863252-GCST004618-EFO_0005090.h.tsv 27863252-GCST004618.tsv
+mv 27863252-GCST004620-EFO_0004833.h.tsv 27863252-GCST004620.tsv
+mv 27863252-GCST004623-EFO_0007994.h.tsv 27863252-GCST004623.tsv
+mv 27863252-GCST004624-EFO_0005090.h.tsv 27863252-GCST004624.tsv
+mv 27863252-GCST004625-EFO_0005091.h.tsv 27863252-GCST004625.tsv
+mv 27863252-GCST004626-EFO_0007988.h.tsv 27863252-GCST004626.tsv
+mv 27863252-GCST004627-EFO_0004587.h.tsv 27863252-GCST004627.tsv
+mv 27863252-GCST004629-EFO_0004833.h.tsv 27863252-GCST004629.tsv
+mv 27863252-GCST004631-EFO_0007992.h.tsv 27863252-GCST004631.tsv
+mv 27863252-GCST004632-EFO_0007993.h.tsv 27863252-GCST004632.tsv
+mv 27863252-GCST004633-EFO_0007990.h.tsv 27863252-GCST004633.tsv
+mv 27863252-GCST004634-EFO_0007995.h.tsv 27863252-GCST004634.tsv
